5978b1e736f7c22e4184d16f297ab4c527bffaab
Salaheddine <salahsimbaa@gmail.com>
2025-11-25 18:03:42 +0100

    Add card and container

---
+++ src/components/Todolist.js
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+export default function Todolist() {
+  return (
+      <Container maxWidth="sm">
+        <CardContent>
+      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        salam salah
+      </Typography>
+      <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+
+      </Container>
+
+  );
+}
