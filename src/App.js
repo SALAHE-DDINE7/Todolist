43d9797cc1a78961535124f4f4c5c688d4de9b5e
Salaheddine <salahsimbaa@gmail.com>
2025-11-26 09:44:52 +0100

    Build the Structure of the Project

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Todolist from './components/Todolist';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center',
+    background: 'lightgray',
+    height:'100vh',}} >
       <Todolist />
           </div>
   );
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -4,29 +4,34 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Divider from '@mui/material/Divider';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 
 export default function Todolist() {
   return (
-      <Container maxWidth="sm">
-        <CardContent>
-      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-        Word of the Day
+  <Container maxWidth="sm">
+   <CardContent style={{background:'white'}}>
+          {/* Title */}
+      <Typography variant='h2'  sx={{ color: 'text.secondary'}}>
+       My To Do List
       </Typography>
-      <Typography variant="h5" component="div">
-        salam salah
-      </Typography>
-      <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>adjective</Typography>
-      <Typography variant="body2">
-        well meaning and kindly.
-        <br />
-        {'"a benevolent smile"'}
-      </Typography>
-    </CardContent>
-    <CardActions>
+      {/*======= finish title ======= */}
+       {/* Divider */}
+       <Divider/>
+       {/* Toggle Button filter */}
+    <ToggleButtonGroup exclusive style={{marginTop:"15px", marginBottom:"15px"}} >
+      <ToggleButton value="left">Done</ToggleButton>
+      <ToggleButton value="center">All</ToggleButton>
+      <ToggleButton value="right">Not yet</ToggleButton>
+    </ToggleButtonGroup>
+      {/*====== Finish toggle  Button filter =====*/}
+      <CardActions>
       <Button size="small">Learn More</Button>
-    </CardActions>
+      </CardActions>
+    </CardContent>
 
-      </Container>
+  </Container>
 
   );
 }
