941ec0c6a3f4b5ac9afa3995f38220acebeddb5e
Salaheddine <salahsimbaa@gmail.com>
2025-11-27 10:39:21 +0100

    Start compliting the components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,7 @@ import Grid from '@mui/material/Grid';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import IconButton from '@mui/material/IconButton';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import EditIcon from '@mui/icons-material/Edit';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 
 
 
@@ -14,24 +14,24 @@ import EditIcon from '@mui/icons-material/Edit';
 
 export default function Todo() {
   return (
-  <Container  style={{background:'orange', color: 'white',}}>
+  <Container  style={{background:'orange', color: 'white',padding:'30px'}}>
 
     <Grid container spacing={2} style={{background:'orange'}} >
   <Grid size={8} >
               {/* Title */}
-      <Typography variant='h4' sx={{textAlign:'left',}} >My first To Do Item</Typography>
+      <Typography variant='h4' sx={{textAlign:'left',}} >First Mission</Typography>
   </Grid>
   {/* Action Icons */}
   <Grid size={4} sx={{textAlign:'right',}} >
-<IconButton>
+<IconButton  style={{background: 'white',border:'2px solid green',borderRadius:'50%'}}>
   <AddTaskIcon style={{color:'green',}}/>
 </IconButton>
 
-<IconButton>
-  <EditIcon style={{color:'blue',}}/>
+<IconButton  style={{background: 'white',border:'2px solid blue',borderRadius:'50%'}}>
+  <EditOutlinedIcon style={{color:'blue'}}/>
 </IconButton>
 
-<IconButton>
+<IconButton style={{background: 'white',border:'2px solid red',borderRadius:'50%'}}>
   <DeleteOutlineIcon style={{color:'red',}}/>
 </IconButton>
 {/* ===== Action Icons ===== */}
