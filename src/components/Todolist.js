d0e2186c38e53724c6c4b21f84c54300527af9a8
Salaheddine <salahsimbaa@gmail.com>
2025-12-01 12:01:42 +0100

    Add Todo button

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -8,6 +8,7 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 
 
@@ -18,7 +19,7 @@ import Todo from './Todo';
 import { v4 } from 'uuid';
 
 
-const todo = [
+const todos = [
   {
     id: v4(),
     title: "Complete Python",
@@ -43,9 +44,31 @@ const todo = [
 
 
 export default function Todolist() {
+  // States
+  const [todo,setTodos]=useState(todos);
+  const [inputValue, setInputValue]=useState("");
+  // Affichage des todos
   const todoaffiche = todo.map((e)=>{
     return <Todo key={e.id} title={e.title} details={e.details} />;
   })
+
+  // Functions
+function handleAddTodo(){
+  const newTodo = {
+    id:v4(),
+    title : inputValue,
+    details : "",
+    isCompleted : false,
+  }
+  setTodos([...todo, newTodo]);
+  setInputValue("");
+}
+
+
+  // ====== finish functions ====
+
+
+
   return (
   <Container maxWidth="sm">
    <CardContent style={{background:'white'}}>
@@ -72,11 +95,18 @@ export default function Todolist() {
     <Grid container spacing={2} sx={{marginTop:"20px"}}>
 
   <Grid size={8}>
-<TextField id="outlined-basic" label="Add Task" variant="outlined" sx={{width:"100%"}} />
+<TextField id="outlined-basic" label="Add Task" variant="outlined" sx={{width:"100%"}} 
+value={inputValue}
+onChange={(e)=>setInputValue(e.target.value)}
+/>
   </Grid>
 
   <Grid size={4} sx={{textAlign:'right',}} display="flex" justifyContent="space-around" alignItems="center">
-      <Button variant="contained" color="success" sx={{width:"90%",height:"90%"}}>
+      <Button variant="contained" color="success" sx={{width:"90%",height:"90%"}}
+      onClick={()=>{
+        handleAddTodo();
+      }}
+      >
         Add
       </Button>
 </Grid>
